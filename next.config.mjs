e5aa238f3605373fbfcc18be93609033e5e916d1
JungWoo Ham <evaham@naver.com>
2024-06-11 09:39:42 +0900

    240611_2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+      unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
+    },
+    compiler: {
+      styledComponents: true, // styled-components 사용 시 컴파일러에 추가
+    },
+};
 export default nextConfig;
