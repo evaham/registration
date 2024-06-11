58ce97f638ab21e19ad606e9d6a874e60146dbce
JungWoo Ham <evaham@naver.com>
2024-06-11 09:12:28 +0900

    20240611

--- src/app/page.js
+++ src/app/page.js
@@ -2,112 +2,105 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="">
+      <div className="p-4 py-10">
+        <p className="text-4xl leading-tight font-bold tracking-tighter text-slate-700">투게더마트</p>
+        <p className="text-4xl leading-tight font-bold tracking-tighter text-slate-700"><span className="text-blue-600 italic mr-3">SPEED</span> 회원가입</p>
+        <p className="text-slate-500">137-81-39709</p>
+      </div>
+      <div className="flex font-bold border border-b-0 border-slate-200">
+        <a href="" className="flex-1 py-4 bg-white text-center">일반회원</a>
+        <a href="" className="flex-1 py-4 bg-slate-200 text-slate-400 text-center">사업자회원</a>
+      </div>
+
+
+      <div>
+        <div className="p-4 bg-white">
+          <p className="flex mt-4 text-slate-600 items-center">필수 입력사항</p>
+          <div className="my-4">
+            <label for="name" class="block text-sm leading-6 text-slate-500 font-bold">고객명</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="text" name="name" id="name" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="고객명을 입력하세요"/>
+            </div>
+          </div>
+          <div className="my-4">
+            <label for="phone" class="block text-sm leading-6 text-slate-500 font-bold">휴대폰번호</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="tel" name="phone" id="phone" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="휴대폰 번호를 입력하세요"/>
+              <div class="absolute inset-y-0 right-0 flex items-center"></div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-3 p-4 bg-white">
+          <p className="flex my-3 text-slate-600 items-center">선택입력사항</p>
+          <div className="my-4">
+            <label for="" class="block text-sm leading-6 text-slate-500 font-bold">주소</label>
+            <div className="mt-1">
+              <a href="" className="block w-2/4 p-2 border text-center bg-blue-500 text-white">우편번호 검색</a>
+            </div>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="text" name="price" id="adress" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder=""/>
+            </div>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="text" id="adressText" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="상세주소를 입력해주세요"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div>
+        <div className="p-4 bg-white">
+          <p className="flex mt-4 text-slate-600 items-center">필수 입력사항</p>
+          <div className="my-4">
+            <label for="name" class="block text-sm leading-6 text-slate-500 font-bold">고객명</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="text" name="name" id="name" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="고객명을 입력하세요"/>
+            </div>
+          </div>
+          <div className="my-4">
+            <label for="phone" class="block text-sm leading-6 text-slate-500 font-bold">휴대폰번호</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="tel" name="phone" id="phone" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="휴대폰 번호를 입력하세요"/>
+            </div>
+          </div>
+          <div className="my-4">
+            <label for="phone" class="block text-sm leading-6 text-slate-500 font-bold">사업자상호</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="tel" name="phone" id="phone" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="사업자 상호를 입력하세요"/>
+            </div>
+          </div>
+          <div className="my-4">
+            <label for="phone" class="block text-sm leading-6 text-slate-500 font-bold">사업자번호</label>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="tel" name="phone" id="phone" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="휴대폰 번호를 입력하세요"/>
+            </div>
+          </div>
+
+
+        </div>
+        <div className="mt-3 p-4 bg-white">
+          <p className="flex my-3 text-slate-600 items-center">선택입력사항</p>
+          <div className="my-4">
+            <label for="" class="block text-sm leading-6 text-slate-500 font-bold">주소</label>
+            <div className="mt-1">
+              <a href="" className="block w-2/4 p-2 border text-center bg-blue-500 text-white">우편번호 검색</a>
+            </div>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="text" name="price" id="adress" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder=""/>
+            </div>
+            <div class="relative mt-1 rounded-md shadow-sm">
+              <input type="text" id="adressText" class="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="상세주소를 입력해주세요"/>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+
+
+      <div className="mt-3 p-4 bg-white">
+        <a href="#" className="block p-3 border text-center bg-blue-600 text-white">입력완료</a>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
