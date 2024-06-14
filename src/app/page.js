fe4fca99c5582a77c90060f34747c81c4d782886
JungWoo Ham <evaham@naver.com>
2024-06-14 11:58:57 +0900

    2024-06-14_last

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,7 @@ export default function Home() {
   };
 
 
+  // 모달창
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -31,6 +32,31 @@ export default function Home() {
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
+  };
+
+
+  // 약관동의
+  const [agreeAll, setAgreeAll] = useState(false);
+  const [requiredAgreed, setRequiredAgreed] = useState(true);
+  const [optionalAgreed, setOptionalAgreed] = useState(false);
+
+  const handleAgreeAllChange = () => {
+    const newAgreeAll = !agreeAll;
+    setAgreeAll(newAgreeAll);
+    setRequiredAgreed(newAgreeAll);
+    setOptionalAgreed(newAgreeAll);
+  };
+
+  const handleRequiredChange = () => {
+    const newRequiredAgreed = !requiredAgreed;
+    setRequiredAgreed(newRequiredAgreed);
+    setAgreeAll(newRequiredAgreed && optionalAgreed);
+  };
+
+  const handleOptionalChange = () => {
+    const newOptionalAgreed = !optionalAgreed;
+    setOptionalAgreed(newOptionalAgreed);
+    setAgreeAll(newOptionalAgreed && requiredAgreed);
   };
 
 
@@ -115,10 +141,9 @@ export default function Home() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className="items-center w-4 h-4 fill-pink-500 ml-1">
                     <path xmlns="http://www.w3.org/2000/svg" d="M440-120v-264L254-197l-57-57 187-186H120v-80h264L197-706l57-57 186 187v-264h80v264l186-187 57 57-187 186h264v80H576l187 186-57 57-186-187v264h-80Z"/>                
                   </svg>
-
                 </label>
                 <div className="relative mt-1 rounded-md shadow-sm">
-                  <input type="tel" name="phone" id="phone" className="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="사업자 상호를 입력하세요"/>
+                  <input type="tel" name="text" id="phone" className="block w-full rounded-md border border-slate-300 p-1.5 text-gray-900 outline-none  placeholder:text-gray-400 focus:border-blue-600" placeholder="사업자 상호를 입력하세요"/>
                 </div>
               </div>
               <div className="my-4">
@@ -178,31 +203,34 @@ export default function Home() {
         <p className="flex my-3 text-slate-600 items-center tracking-tighter">약관동의</p>
         <div className="mt-1 border rounded-md bg-slate-50">
           <div className="flex border-b p-4">
-            <a href="" className="flex">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed" className="fill-slate-300 mr-2" >
+            <label className="flex">
+              <input type="checkbox" checked={agreeAll} onChange={handleAgreeAllChange} className="hidden peer"/>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed" className="fill-slate-300 mr-2 peer-checked:fill-blue-500" >
                 <path d="M423.28-291.22 708.87-576.8l-62.46-62.7-223.13 223.13L312.15-527.5l-62.45 62.7 173.58 173.58ZM480-71.87q-84.91 0-159.34-32.12-74.44-32.12-129.5-87.17-55.05-55.06-87.17-129.5Q71.87-395.09 71.87-480t32.12-159.34q32.12-74.44 87.17-129.5 55.06-55.05 129.5-87.17 74.43-32.12 159.34-32.12t159.34 32.12q74.44 32.12 129.5 87.17 55.05 55.06 87.17 129.5 32.12 74.43 32.12 159.34t-32.12 159.34q-32.12 74.44-87.17 129.5-55.06 55.05-129.5 87.17Q564.91-71.87 480-71.87Z"/>
               </svg>
               모두동의
-            </a>
+            </label>
           </div>
           <ul className="p-4">
             <li className="flex items-center">
-              <a href="" className="flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-blue-500 mr-2" >
+              <label className="flex items-center">
+                <input type="checkbox" checked={requiredAgreed} onChange={handleRequiredChange} className="hidden peer" />
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-slate-300 mr-2 peer-checked:fill-blue-500" >
                   <path d="M423.28-291.22 708.87-576.8l-62.46-62.7-223.13 223.13L312.15-527.5l-62.45 62.7 173.58 173.58ZM480-71.87q-84.91 0-159.34-32.12-74.44-32.12-129.5-87.17-55.05-55.06-87.17-129.5Q71.87-395.09 71.87-480t32.12-159.34q32.12-74.44 87.17-129.5 55.06-55.05 129.5-87.17 74.43-32.12 159.34-32.12t159.34 32.12q74.44 32.12 129.5 87.17 55.05 55.06 87.17 129.5 32.12 74.43 32.12 159.34t-32.12 159.34q-32.12 74.44-87.17 129.5-55.06 55.05-129.5 87.17Q564.91-71.87 480-71.87Z"/>
                 </svg>                   
                 이용약관 <span className="ml-1 text-blue-600">(필수)</span>
-              </a>
-              <button className="text-sm ml-auto underline" onClick={handleOpenModal}>내용보기</button>
+              </label>
+              <button className="text-xs ml-auto underline" onClick={handleOpenModal}>내용보기</button>
             </li>
             <li className="flex items-center mt-4">
-              <a href="" className="flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-slate-300 mr-2">
+              <label className="flex items-center">
+                <input type="checkbox" checked={optionalAgreed} onChange={handleOptionalChange} className="hidden peer" />
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-slate-300 mr-2 peer-checked:fill-blue-500">
                   <path d="M423.28-291.22 708.87-576.8l-62.46-62.7-223.13 223.13L312.15-527.5l-62.45 62.7 173.58 173.58ZM480-71.87q-84.91 0-159.34-32.12-74.44-32.12-129.5-87.17-55.05-55.06-87.17-129.5Q71.87-395.09 71.87-480t32.12-159.34q32.12-74.44 87.17-129.5 55.06-55.05 129.5-87.17 74.43-32.12 159.34-32.12t159.34 32.12q74.44 32.12 129.5 87.17 55.05 55.06 87.17 129.5 32.12 74.43 32.12 159.34t-32.12 159.34q-32.12 74.44-87.17 129.5-55.06 55.05-129.5 87.17Q564.91-71.87 480-71.87Z"/>
-                </svg>                    
-                쿠폰, 특가상품 이벤트 등 혜택/정보 <span className="ml-1 text-blue-600">(선택)</span>
-              </a>
-              <button className="text-sm ml-auto underline" onClick={handleOpenModal}>내용보기</button>
+                </svg>
+                <span>쿠폰, 특가상품 이벤트 등 <br/>혜택/정보 수신<span className="ml-1 text-blue-600">(선택)</span></span>
+              </label>
+              <button className="text-xs ml-auto underline" onClick={handleOpenModal}>내용보기</button>
             </li>
           </ul>
         </div>
