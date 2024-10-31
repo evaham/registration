c4bca71b44b5d9ffa2e6d011abad72675057203d
JungWoo Ham <evaham@naver.com>
2024-10-31 11:01:45 +0900

    20241031_4

--- src/app/page.js
+++ src/app/page.js
@@ -277,26 +277,20 @@ export default function Home() {
 
       {activePage === 1 &&(
         <div className="fixed bottom-0 left-0 right-0 flex px-4 py-3 gap-2 bg-white shadow-2xl">
-          <div className="max-w-96 mx-auto mb-24">
-            <button className="block w-full p-3 border border-[#06CC99] rounded-md text-slate-600">취소</button>
-            <button onClick={()=>handlePageClick(2)} className="block w-full p-3 border bg-[#06CC99] rounded-md text-white">다시 연결</button>
-          </div>
+          <button className="block w-full p-3 border border-[#06CC99] rounded-md text-slate-600">취소</button>
+          <button onClick={()=>handlePageClick(2)} className="block w-full p-3 border bg-[#06CC99] rounded-md text-white">다시 연결</button>
         </div>
       )}
       {activePage === 2 &&(
         <div className="fixed bottom-0 left-0 right-0 flex px-4 py-3 gap-2 bg-white shadow-2xl">
-          <div  className="max-w-96 mx-auto mb-24">
-            <button onClick={()=>handlePageClick(1)} className="block w-full p-3 border border-[#06CC99] rounded-md text-slate-600">취소</button>
-            <button onClick={()=>handlePageClick(3)} className="block w-full p-3 border bg-[#06CC99] rounded-md text-white">동의하고 회원가입</button>
-          </div>
+          <button onClick={()=>handlePageClick(1)} className="block w-full p-3 border border-[#06CC99] rounded-md text-slate-600">취소</button>
+          <button onClick={()=>handlePageClick(3)} className="block w-full p-3 border bg-[#06CC99] rounded-md text-white">동의하고 회원가입</button>
         </div>
       )}
       {activePage === 3 &&(
         <div className="fixed bottom-0 left-0 right-0 flex px-4 py-3 gap-2 bg-white shadow-2xl">
-          <div  className="max-w-96 mx-auto mb-24">
-            <button onClick={()=>handlePageClick(2)} className="block w-full p-3 border border-[#06CC99] rounded-md text-slate-600">취소</button>
-            <button disabled={!isButtonEnabled} className={`block w-full p-3 border text-center rounded-md ${isButtonEnabled ? 'bg-[#06CC99] text-white' : 'bg-slate-100 text-slate-400'}`} >저장</button>
-          </div>
+          <button onClick={()=>handlePageClick(2)} className="block w-full p-3 border border-[#06CC99] rounded-md text-slate-600">취소</button>
+          <button disabled={!isButtonEnabled} className={`block w-full p-3 border text-center rounded-md ${isButtonEnabled ? 'bg-[#06CC99] text-white' : 'bg-slate-100 text-slate-400'}`} >저장</button>
         </div>
       )}
     </div>
